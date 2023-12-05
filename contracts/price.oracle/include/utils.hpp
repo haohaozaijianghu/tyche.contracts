32ff6b5f4e5a104d5683e905e3cf275929dc3acc
bull <bullbull@gmail.com>
2023-12-05 18:25:09 +0800

    add oracle

---
+++ contracts/price.oracle/include/utils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <eosio/eosio.hpp>
+
+#include <eosio/asset.hpp>
+
+using namespace std;
+
+string_view trim(string_view sv) {
+    sv.remove_prefix(std::min(sv.find_first_not_of(" "), sv.size())); // left trim
+    sv.remove_suffix(std::min(sv.size()-sv.find_last_not_of(" ")-1, sv.size())); // right trim
+    return sv;
+}
+
+vector<string_view> split(string_view str, string_view delims = " ")
+{
+    vector<string_view> res;
+    std::size_t current, previous = 0;
+    current = str.find_first_of(delims);
+    while (current != std::string::npos) {
+        res.push_back(trim(str.substr(previous, current - previous)));
+        previous = current + 1;
+        current = str.find_first_of(delims, previous);
+    }
+    res.push_back(trim(str.substr(previous, current - previous)));
+    return res;
+}
